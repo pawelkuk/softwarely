a7daf62b7293d61157895e1f666e418f65cc4dc5
Paweł Kukliński <paffcio314@gmail.com>
2021-01-26 20:50:39 +0100

    Add correct collapse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 let json = [
   {
     type: "radio",
     title: "some title",
     data: ["1data1", "1data2"],
+  },
+  {
+    type: "radio",
+    title: "some title x",
+    data: ["1data1x", "1data2x"],
   },
   {
     type: "checkbox",
@@ -54,17 +60,35 @@ let json = [
 ];
 
 const NestedList = ({ json, nestLevel = 0 }) => {
+  const handleClick = (e, type, idx) => {
+    setIfChecked(
+      ifChecked.map(([t, val], index) => {
+        if (type === "checkbox") {
+          if (idx === index) return [t, !val];
+          else return [t, val];
+        } else if (type === "radio" && t === "radio") {
+          if (idx === index) return [t, true];
+          else return [t, false];
+        } else if (type === "radio" && t === "checkbox") return [t, val];
+        else return ["error", "error"];
+      })
+    );
+  };
+  let [ifChecked, setIfChecked] = useState(json.map((el) => [el.type, false]));
   nestLevel++;
   return json.map((el, idx) => {
     const containsNestedList = typeof el === "object";
     if (containsNestedList)
       return (
-        <details>
+        <details open={ifChecked[idx][1]}>
           <summary>
             <input
+              className="clickable"
               type={el.type}
               id={`${el.type}-list-${idx}`}
               name={`${el.type}-list-${nestLevel}`}
+              checked={ifChecked[idx][1]}
+              onChange={(e) => handleClick(e, el.type, idx)}
             />
             {el.title}
           </summary>
